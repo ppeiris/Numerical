f4dccccded4a31d1f274216772ceb49bb98ba302
Prabath Peiris <peiris.prabath@gmail.com>
2015-08-25 16:44:09 -0400

    Add a makefile

--- Algorithems/inversion.C
+++ Algorithems/inversion.C
@@ -1,30 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
 
-
-
-
-int main(int argc, char * argv[])
+int main()
 {
-
-	/* read the data from the file */
-
-	FILE *file;
-	int readCount = 100;
-	file = fopen("IntegerArray.txt", "r");
-	int *data = (int *)malloc(sizeof(int) * readCount);
-
-	for (int i = 0; i < readCount; i++) {
-		fread(file, "%d", &data[i]);
-	}
-
-
-
-	/* print the initial array */
-
-	/* merge sort */
-
-	/* print the sorted array */
-
-	return 0;
+  printf("This is a test");
+  return 0;
 }
