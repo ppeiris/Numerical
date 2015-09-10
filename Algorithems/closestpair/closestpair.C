2ce8e0fc3b5131acde15ddd0d5f30151084aa764
Prabath Peiris <peiris.prabath@gmail.com>
2015-09-09 22:05:23 -0400

    Define arrays for X and Y

--- Algorithems/closestpair/closestpair.C
+++ Algorithems/closestpair/closestpair.C
@@ -1,40 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "closestpair.h"
+
+void sortXY(Point *data, int start, int mid, int end)
+{
+
+		printf("[%d %d %d]\n", start, mid, end);
+
+		int *X = (int *)malloc(sizeof(int) * (end - start + 1)); // X values
+		int *Y = (int *)malloc(sizeof(int) * (end - start + 1)); // Y values
+
+
+		int leftCount = mid - start + 1;
+		int rightCount = end - mid;
+
+		for (int i = 0; i < leftCount; i++) {
+			*(X + i) =
+		}
+
+
+
+}
+
+
+void findClosestPair(Point *data, int start, int end)
+{
+	if (start < end) {
+		int mid = start + (end - start)/2;
+		findClosestPair(data, start, mid);
+		findClosestPair(data, (mid + 1), end);
+		sortXY(data, start, mid, end);
+	}
+}
+
 
 int main()
 {
-	FILE *file = fopen("points.txt", "r");
-
-
 	int readCount = 10;
 
-	typedef struct _point{
-		int x;
-		int y;
-	} Point;
-
+	FILE *file = fopen("points.txt", "r");
 	Point *pointsList = (Point *)malloc(sizeof(Point) * readCount);
-
-
-	pointsList->x = 123;
-	pointsList->y = 434;
-
-	printf("%d %d\n", pointsList->x, pointsList->y);
-
-
-	(pointsList + 1)->x = 234;
-	(pointsList + 1)->y = 999;
-
-
-	printf("%d %d\n", (pointsList + 1)->x, (pointsList + 1)->y);
 
 	for(int i = 0; i < readCount; i++) {
 		fscanf(file, "%d %d",&((pointsList + i)->x), &((pointsList + i)->y));
-		printf("[ %d, %d ]\n", (pointsList + i)->x, (pointsList + i)->y);
+		// printf("[ %d, %d ]\n", (pointsList + i)->x, (pointsList + i)->y);
 
 	}
 
 
+	findClosestPair(pointsList, 0, readCount - 1);
 
 
 	return 0;
---
+++ Algorithems/closestpair/closestpair.h
@@ -0,0 +1,4 @@
+typedef struct _point{
+    int x;
+    int y;
+} Point;
