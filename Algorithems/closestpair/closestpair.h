1c9d0b3f6c3aa1ae5f6d95a0f657303505c4b566
Prabath Peiris <peiris.prabath@gmail.com>
2015-11-14 19:54:15 -0500

    Move all the Algorithems to a new repo

--- Algorithems/closestpair/closestpair.h
+++
@@ -1,9 +0,0 @@
-typedef struct _point {
-    int x;
-    int y;
-    int p;
-} Point;
-
-typedef struct _datalist {
-	Point *datapoint;
-} DataList;
--- Algorithems/quicksort/qsort.h
+++
@@ -1,9 +0,0 @@
-
-
-typedef struct _point {
-  int x;
-} Point;
-
-typedef struct _datalist {
-  Point *datapoint;
-} DataList;
